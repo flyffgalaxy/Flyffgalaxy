9c2cde1ee155b476fe2328702ee693a269b68b58
flyffgalaxy <cataosweinstein3@gmail.com>
2026-08-15 19:46:46 +0800

    Create game-core.js

---
+++ game-core.js
@@ -0,0 +1,10 @@
+export let player = { x: 50, y: 150, hp: 100, mp: 50, exp: 0, level: 1, inventory: [] };
+
+export let enemies = [
+  { x: 300, y: 150, hp: 50 },
+  { x: 400, y: 200, hp: 70 },
+  { x: 250, y: 100, hp: 30 }
+];
+
+export let damageTexts = [];
+export let lootDrops = [];
